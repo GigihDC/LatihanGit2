8649c95b4f19c732fa8fc2fd9c61712f0c4ab5b9
Akmaliin <akmalnafis25@gmail.com>
2024-03-01 20:30:25 +0700

    [Task 3] Adding feature Square Class

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,6 @@ class feature_square(val sideLength: Double) {
     fun calculateArea(): Double {
         return sideLength * sideLength
     }
-
     fun calculatePerimeter(): Double {
         return 4 * sideLength
     }
